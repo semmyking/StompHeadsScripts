a3eda64b50a0bd2ed8879249de7a509598c23fe2
Sem Satoer <32928355+semmyking@users.noreply.github.com>
2019-10-16 09:34:04 +0200

    added scripts
    added scripts to the repo

---
+++ SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource sound;
+
+    // Singleton instance.
+    public static SoundManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // Dont destroy when switching scenes 
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        sound.clip = clip;
+        sound.PlayOneShot(clip);
+    }
+}
